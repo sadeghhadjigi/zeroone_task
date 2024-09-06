fd7a016dc198cad910334de340a13490b2021e20
SaddyHadjigi <saddy.hadjigi@gmail.com>
2024-09-06 21:18:35 +0330

    Refactor the app class

--- ZeroOneTask.Application/ZeroOneTaskApp.cs
+++ ZeroOneTask.Application/ZeroOneTaskApp.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using ZeroOneTask.Application.Dtos;
 using ZeroOneTask.Application.Interfaces;
+using ZeroOneTask.Domain.Entities;
 
 namespace ZeroOneTask.Application
 {
@@ -20,54 +21,70 @@ namespace ZeroOneTask.Application
             try
             {
                 var agencySubscriptions = await _subscriptionRepository.GetSubscriptionsByAgencyId(agencyId);
-
                 var inDateTotalFlights = await _flightRepository.GetFlightsByDate(startDate, endDate);
-                var flightsByRoute = inDateTotalFlights
-                                        .GroupBy(x => x.OriginCityId)
-                                        .ToDictionary(group => group.Key, group => group.GroupBy(x => x.DestinationCityId).ToDictionary(g => g.Key, g => g.ToList()));
-
-                var flights = new List<FlightDto>();
-                foreach (var subscription in agencySubscriptions)
-                {
-                    if (flightsByRoute.ContainsKey(subscription.OriginCityId))
-                    {
-                        if (flightsByRoute[subscription.OriginCityId].ContainsKey(subscription.DestinationCityId))
-                        {
-                            flights.AddRange(flightsByRoute[subscription.OriginCityId][subscription.DestinationCityId]);
-                        }
-                    }
-                }
-
-                var flightsByAirlineId = flights
-                                            .GroupBy(x => x.AirlineId)
-                                            .ToDictionary(group => group.Key, group => group.Select(x => x.DepartureTime).ToList());
-
-                var validFlights = flights.Where(flight => flight.DepartureTime >= startDate && flight.DepartureTime <= endDate);
-                foreach (var flight in validFlights)
-                {
-                    flight.Status = !flightsByAirlineId[flight.AirlineId].Any(f => f >= flight.DepartureTime.AddDays(-7).AddHours(-0.5) &&
-                        f <= flight.DepartureTime.AddDays(-7).AddHours(0.5)) ? "New"
-                        : !flightsByAirlineId[flight.AirlineId].Any(f => f >= flight.DepartureTime.AddDays(7).AddHours(-0.5) &&
-                        f <= flight.DepartureTime.AddDays(7).AddHours(0.5)) ? "Discontinued"
-                        : "";
-                }
-
-                var finalResult = flights.Where(f => !string.IsNullOrEmpty(f.Status)).ToList();
-
-                var filePath = "result.csv";
-                using (var writer = new StreamWriter(filePath, false, Encoding.UTF8))
-                {
-                    writer.WriteLine("flight_id,origin_city_id,destination_city_id,departure_time,arrival_time,airline_id,status");
-
-                    foreach (var row in finalResult)
-                    {
-                        writer.WriteLine($"{row.FlightId},{row.OriginCityId},{row.DestinationCityId},{row.DepartureTime},{row.ArrivalTime},{row.AirlineId},{row.Status}");
-                    }
-                }
+                var flights = PrepareAgencyFlights(agencySubscriptions, inDateTotalFlights);
+                var results = PrepareChangeDetectionFlights(flights, startDate, endDate);
+                GenerateCSV("result.csv", results);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+            }
+        }
+
+        private List<FlightDto> PrepareAgencyFlights(List<Subscription> agencySubscriptions, List<FlightDto> inDateTotalFlights)
+        {
+            var flightsByRouteDict = inDateTotalFlights
+                    .GroupBy(x => x.OriginCityId)
+                    .ToDictionary(group => group.Key, group => group.GroupBy(x => x.DestinationCityId).ToDictionary(g => g.Key, g => g.ToList()));
+
+            var flights = new List<FlightDto>();
+            foreach (var subscription in agencySubscriptions)
+            {
+                if (flightsByRouteDict.ContainsKey(subscription.OriginCityId))
+                {
+                    if (flightsByRouteDict[subscription.OriginCityId].ContainsKey(subscription.DestinationCityId))
+                    {
+                        flights.AddRange(flightsByRouteDict[subscription.OriginCityId][subscription.DestinationCityId]);
+                    }
+                }
+            }
+
+            return flights;
+        }
+
+        private List<FlightDto> PrepareChangeDetectionFlights(List<FlightDto> flights, DateTime startDate, DateTime endDate)
+        {
+            var flightsByAirlineIdDict = flights
+                    .GroupBy(x => x.AirlineId)
+                    .ToDictionary(group => group.Key, group => group.Select(x => x.DepartureTime).ToList());
+
+            var validFlights = flights.Where(flight => flight.DepartureTime >= startDate && flight.DepartureTime <= endDate);
+            foreach (var flight in validFlights)
+            {
+                var lastWeekFlightDepartureTime = flight.DepartureTime.AddDays(-7);
+                var nextWeekFlightDepartureTime = flight.DepartureTime.AddDays(7);
+
+                flight.Status = !flightsByAirlineIdDict[flight.AirlineId].Any(f => f >= lastWeekFlightDepartureTime.AddHours(-0.5) &&
+                    f <= lastWeekFlightDepartureTime.AddHours(0.5)) ? "New"
+                    : !flightsByAirlineIdDict[flight.AirlineId].Any(f => f >= nextWeekFlightDepartureTime.AddHours(-0.5) &&
+                    f <= nextWeekFlightDepartureTime.AddHours(0.5)) ? "Discontinued"
+                    : "";
+            }
+
+            return flights.Where(f => !string.IsNullOrEmpty(f.Status)).ToList();
+        }
+
+        private void GenerateCSV(string filePath, List<FlightDto> results)
+        {
+            using (var writer = new StreamWriter(filePath, false, Encoding.UTF8))
+            {
+                writer.WriteLine("flight_id,origin_city_id,destination_city_id,departure_time,arrival_time,airline_id,status");
+
+                foreach (var row in results)
+                {
+                    writer.WriteLine($"{row.FlightId},{row.OriginCityId},{row.DestinationCityId},{row.DepartureTime},{row.ArrivalTime},{row.AirlineId},{row.Status}");
+                }
             }
         }
     }
